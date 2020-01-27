ddf66bb2f5668bc0f2e2e4ca79a8315ea3f6116c
Adrian Guerra <2adrian2@gmail.com>
2020-01-27 11:33:39 -0800

    Update camera.py
    clamp x rotation
    wrap y & z rotation
    remove unused attributes

--- raymarch/camera.py
+++ raymarch/camera.py
@@ -4,10 +4,8 @@ import numpy as np
 
 class Camera:
     # TODO move this class to a more relevant subpackage of raymarch
-    def __init__(self, fov: float = 80, near: float = 0.1, far: float = 100):
+    def __init__(self, fov: float = 80):
         self.fov = fov
-        self.near = near
-        self.far = far
         self.rotation = np.array([0, 0, 0], dtype=float)
         self.location = np.array([0, 0, 0], dtype=float)
 
@@ -15,9 +13,9 @@ class Camera:
         sx, sy, sz = np.sin(self.rotation)
         cx, cy, cz = np.cos(self.rotation)
         return np.array([
-            (cy*cz, -cy*sz, sy),
-            (cx*sz+sx*sy*cz, cx*cz-sx*sy*sz, -sx*cy),
-            (sx*sz-cx*sy*cz, sx*cz-cx*sy*sz, cx*cy)
+            (cy * cz, -cy * sz, sy),
+            (cx * sz + sx * sy * cz, cx * cz - sx * sy * sz, -sx * cy),
+            (sx * sz - cx * sy * cz, sx * cz - cx * sy * sz, cx * cy)
         ])
 
     def move_by(self, movement_vector):
@@ -47,6 +45,7 @@ class Camera:
     @z.setter
     def z(self, z):
         self.location[2] = z
+
     # endregion
 
     # region rotation getters/setters
@@ -56,7 +55,7 @@ class Camera:
 
     @rotation_x.setter
     def rotation_x(self, n):
-        self.rotation[0] = n
+        self.rotation[0] = np.clip(n, a_min=-math.pi / 2, a_max=math.pi / 2)
 
     @property
     def rotation_y(self):
@@ -64,7 +63,7 @@ class Camera:
 
     @rotation_y.setter
     def rotation_y(self, n):
-        self.rotation[1] = n
+        self.rotation[1] = math.fmod(n, math.tau)
 
     @property
     def rotation_z(self):
@@ -72,5 +71,5 @@ class Camera:
 
     @rotation_z.setter
     def rotation_z(self, n):
-        self.rotation[2] = n
+        self.rotation[2] = math.fmod(n, math.tau)
     # endregion
