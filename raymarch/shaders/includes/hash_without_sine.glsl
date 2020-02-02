3dd94e955b0ebd17b8a689ac074bb124a218ad68
Adrian Guerra <2adrian2@gmail.com>
2020-02-02 10:36:46 -0800

    rename float hash functions

--- raymarch/shaders/includes/hash_without_sine.glsl
+++ raymarch/shaders/includes/hash_without_sine.glsl
@@ -22,128 +22,83 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.*/
 
-//----------------------------------------------------------------------------------------
-//  1 out, 1 in...
-float hash1to1(float p)
-{
-    p = fract(p * .1031);
-    p *= p + 33.33;
-    p *= p + p;
-    return fract(p);
+float hash1fto1f(float p){
+	p = fract(p * .1031);
+	p *= p + 33.33;
+	p *= p + p;
+	return fract(p);
 }
 
-//----------------------------------------------------------------------------------------
-//  1 out, 2 in...
-float hash2to1(vec2 p)
-{
-	vec3 p3  = fract(vec3(p.xyx) * .1031);
-    p3 += dot(p3, p3.yzx + 33.33);
-    return fract((p3.x + p3.y) * p3.z);
+float hash2fto1f(vec2 p){
+	vec3 p3 = fract(vec3(p.xyx) * .1031);
+	p3 += dot(p3, p3.yzx + 33.33);
+	return fract((p3.x + p3.y) * p3.z);
 }
 
-//----------------------------------------------------------------------------------------
-//  1 out, 3 in...
-float hash3to1(vec3 p3)
-{
-	p3  = fract(p3 * .1031);
-    p3 += dot(p3, p3.yzx + 33.33);
-    return fract((p3.x + p3.y) * p3.z);
+float hash3fto1f(vec3 p3){
+	p3 = fract(p3 * .1031);
+	p3 += dot(p3, p3.yzx + 33.33);
+	return fract((p3.x + p3.y) * p3.z);
 }
 
-//----------------------------------------------------------------------------------------
-//  2 out, 1 in...
-vec2 hash1to2(float p)
-{
+vec2 hash1fto2f(float p){
 	vec3 p3 = fract(vec3(p) * vec3(.1031, .1030, .0973));
 	p3 += dot(p3, p3.yzx + 33.33);
-    return fract((p3.xx+p3.yz)*p3.zy);
-
+	return fract((p3.xx+p3.yz)*p3.zy);
 }
 
-//----------------------------------------------------------------------------------------
-///  2 out, 2 in...
-vec2 hash2to2(vec2 p)
-{
+vec2 hash2fto2f(vec2 p){
 	vec3 p3 = fract(vec3(p.xyx) * vec3(.1031, .1030, .0973));
-    p3 += dot(p3, p3.yzx+33.33);
-    return fract((p3.xx+p3.yz)*p3.zy);
-
+	p3 += dot(p3, p3.yzx+33.33);
+	return fract((p3.xx+p3.yz)*p3.zy);
 }
 
-//----------------------------------------------------------------------------------------
-///  2 out, 3 in...
-vec2 hash3to2(vec3 p3)
-{
+vec2 hash3fto2f(vec3 p3){
 	p3 = fract(p3 * vec3(.1031, .1030, .0973));
-    p3 += dot(p3, p3.yzx+33.33);
-    return fract((p3.xx+p3.yz)*p3.zy);
+	p3 += dot(p3, p3.yzx+33.33);
+	return fract((p3.xx+p3.yz)*p3.zy);
 }
 
-//----------------------------------------------------------------------------------------
-//  3 out, 1 in...
-vec3 hash1to3(float p)
-{
-   vec3 p3 = fract(vec3(p) * vec3(.1031, .1030, .0973));
-   p3 += dot(p3, p3.yzx+33.33);
-   return fract((p3.xxy+p3.yzz)*p3.zyx);
+vec3 hash1fto3f(float p){
+	vec3 p3 = fract(vec3(p) * vec3(.1031, .1030, .0973));
+	p3 += dot(p3, p3.yzx+33.33);
+	return fract((p3.xxy+p3.yzz)*p3.zyx);
 }
 
-
-//----------------------------------------------------------------------------------------
-///  3 out, 2 in...
-vec3 hash2to3(vec2 p)
-{
+vec3 hash2fto3f(vec2 p){
 	vec3 p3 = fract(vec3(p.xyx) * vec3(.1031, .1030, .0973));
-    p3 += dot(p3, p3.yxz+33.33);
-    return fract((p3.xxy+p3.yzz)*p3.zyx);
+	p3 += dot(p3, p3.yxz+33.33);
+	return fract((p3.xxy+p3.yzz)*p3.zyx);
 }
 
-//----------------------------------------------------------------------------------------
-///  3 out, 3 in...
-vec3 hash3to3(vec3 p3)
-{
+vec3 hash3fto3f(vec3 p3){
 	p3 = fract(p3 * vec3(.1031, .1030, .0973));
-    p3 += dot(p3, p3.yxz+33.33);
-    return fract((p3.xxy + p3.yxx)*p3.zyx);
-
+	p3 += dot(p3, p3.yxz+33.33);
+	return fract((p3.xxy + p3.yxx)*p3.zyx);
 }
 
-//----------------------------------------------------------------------------------------
-// 4 out, 1 in...
-vec4 hash1to4(float p)
-{
+vec4 hash1fto4f(float p){
 	vec4 p4 = fract(vec4(p) * vec4(.1031, .1030, .0973, .1099));
-    p4 += dot(p4, p4.wzxy+33.33);
-    return fract((p4.xxyz+p4.yzzw)*p4.zywx);
-
+	p4 += dot(p4, p4.wzxy+33.33);
+	return fract((p4.xxyz+p4.yzzw)*p4.zywx);
 }
 
-//----------------------------------------------------------------------------------------
-// 4 out, 2 in...
-vec4 hash2to4(vec2 p)
-{
+vec4 hash2fto4f(vec2 p){
 	vec4 p4 = fract(vec4(p.xyxy) * vec4(.1031, .1030, .0973, .1099));
-    p4 += dot(p4, p4.wzxy+33.33);
-    return fract((p4.xxyz+p4.yzzw)*p4.zywx);
-
+	p4 += dot(p4, p4.wzxy+33.33);
+	return fract((p4.xxyz+p4.yzzw)*p4.zywx);
 }
 
-//----------------------------------------------------------------------------------------
-// 4 out, 3 in...
-vec4 hash3to4(vec3 p)
-{
-	vec4 p4 = fract(vec4(p.xyzx)  * vec4(.1031, .1030, .0973, .1099));
-    p4 += dot(p4, p4.wzxy+33.33);
-    return fract((p4.xxyz+p4.yzzw)*p4.zywx);
+vec4 hash3fto4f(vec3 p){
+	vec4 p4 = fract(vec4(p.xyzx) * vec4(.1031, .1030, .0973, .1099));
+	p4 += dot(p4, p4.wzxy+33.33);
+	return fract((p4.xxyz+p4.yzzw)*p4.zywx);
 }
 
-//----------------------------------------------------------------------------------------
-// 4 out, 4 in...
-vec4 hash4to4(vec4 p4)
-{
-	p4 = fract(p4  * vec4(.1031, .1030, .0973, .1099));
-    p4 += dot(p4, p4.wzxy+33.33);
-    return fract((p4.xxyz+p4.yzzw)*p4.zywx);
+vec4 hash4fto4f(vec4 p4){
+	p4 = fract(p4 * vec4(.1031, .1030, .0973, .1099));
+	p4 += dot(p4, p4.wzxy+33.33);
+	return fract((p4.xxyz+p4.yzzw)*p4.zywx);
 }
 
 #endif
